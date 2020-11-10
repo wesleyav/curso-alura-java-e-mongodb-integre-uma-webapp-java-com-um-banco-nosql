b57fafccda3c7b4d4935eea9a83144a8663b6680
Wesley <wesley.contato@hotmail.com>
2020-11-10 04:44:32 -0300

    Enviando aluno para o Java

--- escolalura/src/main/java/br/com/alura/escolalura/controllers/AlunoController.java
+++ escolalura/src/main/java/br/com/alura/escolalura/controllers/AlunoController.java
@@ -3,6 +3,8 @@ package br.com.alura.escolalura.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import br.com.alura.escolalura.models.Aluno;
 
@@ -13,5 +15,11 @@ public class AlunoController {
 	public String cadastrar(Model model) {
 		model.addAttribute("aluno", new Aluno());
 		return "aluno/cadastrar";
+	}
+	
+	@PostMapping("/aluno/salvar")
+	public String salvar(@ModelAttribute Aluno aluno) {
+		System.out.println("Aluno para salvar: " + aluno);
+		return "redirect:/";
 	}
 }
